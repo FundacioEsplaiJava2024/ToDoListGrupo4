e8a21400617f1de1993f6956227032fc35da1a7f
JordiJT99 <jordijt.99@gmail.com>
2024-06-07 14:02:41 +0200

    Se ha añadido un boton de sumar tareas que suma y resta según vayas añadiendo o eliminando tares. Tanbíen puedes escribir el nombre de la tarea

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,50 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [count, setCount] = useState(0);
+  const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState('');
+
+  const handleInputChange = (event) => {
+    setTaskName(event.target.value);
+  };
+
+  const addTask = () => {
+    if (taskName.trim() !== '') {
+      setTasks([...tasks, { id: tasks.length + 1, name: taskName }]);
+      setTaskName('');
+      setCount(count + 1); // Incrementa el contador al añadir una tarea
+    }
+  };
+
+  const deleteTask = (taskId) => {
+    const updatedTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(updatedTasks);
+    setCount(count -1);
+  };
 
   return (
     <>
-      <div className="parametros"><h2>Idea</h2></div>
-      <div className="parametros"><h2>Iniciando</h2></div>
-      <div className="parametros"><h2>Finalizado</h2></div>
+      <div className="parametros">
+        <h2 className='sub'>Idea <span> {count}</span></h2>
+        <input type="text" value={taskName} onChange={handleInputChange} placeholder="Nombre de la tarea" />
+        <button onClick={addTask} id='aggTarea'>+</button>
+        <div className="listas">
+          
+          <ul>
+            {tasks.map(task => (
+              <li key={task.id}>
+                {task.name}
+                <button onClick={() => deleteTask(task.id)}>Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="parametros" ><h2 className='sub'>Iniciando</h2></div>
+      <div className="parametros"><h2 className='sub'>Finalizado</h2></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
