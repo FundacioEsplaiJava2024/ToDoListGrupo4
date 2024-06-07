020d92fce10a7a415e091db914e6fef43bdbc1d7
AleXKiD31 <alexkid20025@gmail.com>
2024-06-07 13:50:53 +0200

    fixed errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,30 @@ import './App.css'
 function App() {
   const handleDragStart = (event: React.DragEvent<HTMLDivElement>) => {
     event.dataTransfer.setData('text', event.currentTarget.id);
+    event.dataTransfer.effectAllowed = "move";
   }
 
   const enableDropping = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
   }
 
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     const id = event.dataTransfer.getData('text');
+    event.currentTarget.appendChild(document.getElementById(id)!);
     console.log(`Somebody dropped an element with id: ${id}`);
   }
 
   return (
     <>
+      <div className="parametros" onDragOver={enableDropping} onDrop={handleDrop}>
+        <h2>Todo</h2>
       <div className="parametros" id="d1" draggable="true" onDragStart={handleDragStart}><h2>Idea</h2></div>
       <div className="parametros" id="d2" draggable="true" onDragStart={handleDragStart}><h2>Iniciando</h2></div>
       <div className="parametros" id="d3" draggable="true" onDragStart={handleDragStart}><h2>Finalizado</h2></div>
-      <div onDragOver={enableDropping} onDrop={handleDrop}><h2>Drop Area</h2></div>
+      </div>
+      <div className="parametros" onDragOver={enableDropping} onDrop={handleDrop}><h2>En Progreso</h2></div>
+      <div className="parametros" onDragOver={enableDropping} onDrop={handleDrop}><h2>Terminado</h2></div>
     </>
   );
 }
