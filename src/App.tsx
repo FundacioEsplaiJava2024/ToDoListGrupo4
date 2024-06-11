898280b5b919d62c907cf3abc1db02e6351c880b
Ney Apolinario <ney_apolinario1@hotmail.com>
2024-06-11 09:38:05 +0200

    ultima actualizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { useTaskManager } from './Elementos/useTaskManager';
-import Columna from './Columna';
+import { useTaskManager } from './hook/useTaskManager';
+import Columna from './Elementos/Columna';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
