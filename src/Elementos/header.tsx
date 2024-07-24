510fb7ff54df57708499480aefcb7110740362dd
AlexFlFe <alexfloresfelguera@gmail.com>
2024-07-24 10:21:15 +0200

    Fixes

--- src/Elementos/header.tsx
+++ src/Elementos/header.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { useTaskManager } from '../hook/useTaskManager';
 
 const Header: React.FC = () => {
-  const { projectName } = useTaskManager();
+
   
   return (
     <header>
-      <h1 className='titulo1'>{projectName}</h1>
+      <h1 className='titulo1'>{}</h1>
     </header>
   );
 };
